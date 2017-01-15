7da913cfb8d8a9339742f4129d4b1467f5e8095d
Erich Schroeter <erich.schroeter@gmail.com>
2017-01-15 14:49:21 -0600

    Added initial calc.h and .cpp

---
+++ src/calc.cpp
@@ -0,0 +1,6 @@
+#include "calc.h"
+
+int add_int(int first, int second)
+{
+    return first + second;
+}
---
+++ src/calc.h
@@ -0,0 +1,9 @@
+#ifndef __CALC_H__
+#define __CALC_H__
+
+extern "C"
+{
+    int add_int(int first, int second);
+}
+
+#endif /* __CALC_H__ */
